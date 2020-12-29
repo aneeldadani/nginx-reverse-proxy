421fed335a02559d59947bc45432c498d35505cd
Aneel <40775413+aneeldadani@users.noreply.github.com>
2020-12-29 11:20:34 -0800

    Update s3-sync.sh

--- s3-sync.sh
+++ s3-sync.sh
@@ -3,4 +3,7 @@ aws_profile=s3-pcap
 backup_path=/tmp
 bucket_name=my-pcap-bucket
 
+echo "S3 Sync started, sleeping for 30 seconds"
+sleep 30
 aws s3 sync $backup_path s3://$bucket_name --exclude *.log --include *.pcap* --profile=$aws_profile
+echo "S3 Sync ran, check S3 bucket"
